9d1efe0714676027f4fe8437f0590b3890ebfc2c
jgujerry <jgujerry@gmail.com>
2024-11-16 01:24:08 -0700

    Add supervisor config file

--- production/start.sh
+++ production/start.sh
@@ -5,6 +5,7 @@ PROJECT_DIR="/web/$PROJECT_NAME"
 
 PRODRUN_DIR="$PROJECT_DIR/production"
 PRODRUN_SOCKFILE="$PRODRUN_DIR/gunicorn.sock"
+PRODRUN_LOGS_DIR="$PRODRUN_DIR/production/logs"
 
 USER="webuser"
 GROUP="webapp"
@@ -14,13 +15,13 @@ TIMEOUT=120
 NUM_WORKERS=1
 
 cd "$PROJECT_DIR" || exit 1
-echo "Current working directory: $(pwd)"
 source venv/bin/activate
 export DJANGO_SETTINGS_MODULE="config.settings.production"
 export PYTHONPATH="$PROJECT_DIR:$PYTHONPATH"
 
 # Ensure the production directory exists
 test -d "$PRODRUN_DIR" || mkdir -p "$PRODRUN_DIR"
+test -d "$PROJECT_LOGS_DIR" || mkdir -p "$PROJECT_LOGS_DIR"
 
 # Start gunicorn server
 gunicorn "$DJANGO_WSGI_MODULE:application" \
